3ae9e218431f74569104064a11f3464f68255a28
KimSoo <ilppang@naver.com>
2023-06-21 21:50:23 +0900

    led On/Off에 따른 밝기 조절 상황 구분하여 변경

--- spot-light-iot.cpp
+++ spot-light-iot.cpp
@@ -1,5 +1,4 @@
 #include "BluetoothSerial.h"
-
 BluetoothSerial SerialBT;
 
 void setup() {
@@ -9,18 +8,22 @@ void setup() {
 }
 
 void loop() {
+  static bool led_is_on = false;
   if (SerialBT.available()) {
     String message = SerialBT.readString(); // SerialBT로부터 문자열을 읽어옴
-    Serial.println(message);
     if (message == "ON") {
-      analogWrite(17, 255); // 17번 핀에 LOW 신호 출력
-    } else if (message == "OFF") {
-      analogWrite(17, 0); // 17번 핀에 HIGH 신호 출력
-    } else{
-    float duty_cycle = message.toFloat() / 360.0; // 밝기를 나타내는 값 (0 ~ 1)
+      analogWrite(17, 255); // LED ON
+      led_is_on = true;
+    } else if (message == "OFF") { 
+      analogWrite(17, 0); // LED OFF
+      led_is_on = false;
+    } else if (led_is_on) {
+      float duty_cycle = message.toFloat() / 360.0; // 밝기를 나타내는 값 (0 ~ 1)
       analogWrite(17, (int) (duty_cycle * 255)); // PWM Duty 값 설정
+    } else {
+      // LED가 꺼져 있으므로 아무 동작도 수행하지 않음
     }
   }
 
-  delay(100); // 0.1초 대기
+  delay(10); // 0.01초 대기
 }
